0cf3d149f3a0cfd0ad2d015ec7c25ffe301a1fbf
Oussama95el <oussamaelbechari@gmail.com>
2023-02-12 12:05:04 +0100

    initiating spring boot application

---
+++ rhp/src/test/java/com/simplon/rhp/RhpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.rhp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
